87a3c3f13045f25adc748e3cad0f9d5c4283aa99
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-19 00:24:51 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -186,7 +186,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
-      
+
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
